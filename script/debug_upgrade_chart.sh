d823b99d41c09f3c0da2238f277dfd21c716068f
Jordan Pitlor <jordan@pitlor.dev>
2025-12-31 16:52:50 -0500

    Update debug_upgrade_chart.sh

--- script/debug_upgrade_chart.sh
+++ script/debug_upgrade_chart.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
 
-git pull
-git submodule update
-su root -- -x -c '/usr/sbin/qm stop 103 && /usr/sbin/qm destroy 103 && /usr/sbin/qm destroy 901'
 source .env
-
-cd configuration/packer
-packer build -only "proxmox-clone.docker*" .
-sleep 15
-
-cd ../../provisioner
-tofu apply -auto-approve
-sleep 15
-
-cd ../configuration
-ssh-keygen -R 192.168.0.116
-ansible-playbook ./configure-machines.yml
+cd configuration
+git pull origin main
+ansible-playbook ./configure-templates.yml --tags "docker_containers"
