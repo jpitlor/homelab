72522b79fa8cdada9973492f94dce38fd37735db
Jordan Pitlor <jordan@pitlor.dev>
2025-12-30 22:05:13 -0500

    Update debug_upgrade_chart.sh

--- script/debug_upgrade_chart.sh
+++ script/debug_upgrade_chart.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
+git submodule update
 su root -- -x -c '/usr/sbin/qm stop 104 && /usr/sbin/qm destroy 104 && /usr/sbin/qm destroy 901'
 
-cd ~/homelab-configuration
+cd configuration
 source .env
 git pull
 cd packer
 packer build -only "proxmox-clone.docker*" .
-sleep 10
+sleep 15
 
-cd ~/homelab-provisioner
+cd ../provisioner
 tofu apply -auto-approve
-sleep 5
+sleep 15
 
 cd ~/homelab-configuration
 ssh-keygen -R 192.168.0.116
